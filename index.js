42e597ad0cda08b642a99a608765521b45d56264
Zidane Karim <zkarim7676@gmail.com>
2023-02-26 10:43:07 -0500

    changed var to const for imports

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const axios = require("axios");
-const fs = require('fs');
-var checkWord = require('check-if-word'),
+//const fs = require('fs');
+const checkWord = require('check-if-word'),
 words = checkWord('en');
 
 app.use(express.static(__dirname + '/public'));
@@ -14,8 +14,8 @@ app.get("/", function(req, res)  {
 app.use(bodyParser.json());
 
 app.post("/unscramble", async (req, res) => {
-    console.log("POST REQUEST RECEIVED");
-    var text = req.body.data.text;
+    //console.log("POST REQUEST RECEIVED");
+    const text = req.body.data.text;
     let threeLetterWords = [];
     let fourLetterWords = [];
     let fiveLetterWords = [];
@@ -23,7 +23,9 @@ app.post("/unscramble", async (req, res) => {
     let allWords = [];
 
     // find all words with 3 letters
-    
+    function hasVowelOrY(word) {
+        return /[aeiouy]/i.test(word);
+        }
 
     for (let i = 0; i < text.length; i++) {
         for (let j = 0; j < text.length; j++) {
@@ -31,8 +33,9 @@ app.post("/unscramble", async (req, res) => {
             for (let k = 0; k < text.length; k++) {
                 if (i == k || j == k) continue;
                 let word = text[i] + text[j] + text[k];
-                if (word.length < 3 || word.length > 6 || !/[aeiou]/.test(word)) continue;
-                allWords.push(word);
+                if (hasVowelOrY(word)) allWords.push(word);
+                
+                
             }
         }
     }
@@ -46,8 +49,7 @@ app.post("/unscramble", async (req, res) => {
                 for (let l = 0; l < text.length; l++) {
                     if (i == l || j == l || k == l) continue;
                     let word = text[i] + text[j] + text[k] + text[l];
-                    if (word.length < 3 || word.length > 6 || !/[aeiou]/.test(word)) continue;
-                    allWords.push(word);
+                    if (hasVowelOrY(word)) allWords.push(word);
                 }
             }
         }
@@ -64,14 +66,12 @@ app.post("/unscramble", async (req, res) => {
                     for (let m = 0; m < text.length; m++) {
                         if (i == m || j == m || k == m || l == m) continue;
                         let word = text[i] + text[j] + text[k] + text[l] + text[m];
-                        if (word.length < 3 || word.length > 6 || !/[aeiou]/.test(word)) continue;
-                        allWords.push(word);
+                        if (hasVowelOrY(word)) allWords.push(word);
                     }
                 }
             }
         }
     }
-
     // find all words with 6 letters
     for (let i = 0; i < text.length; i++) {
         for (let j = 0; j < text.length; j++) {
@@ -85,85 +85,37 @@ app.post("/unscramble", async (req, res) => {
                         for (let n = 0; n < text.length; n++) {
                             if (i == n || j == n || k == n || l == n || m == n) continue;
                             let word = text[i] + text[j] + text[k] + text[l] + text[m] + text[n];
-                            // check if word has a vowel
-                            if (word.length < 3 || word.length > 6 || !/[aeiou]/.test(word)) continue;
-                            allWords.push(word);
+                            // if word doesn't have a vowel, skip it
+                            if (hasVowelOrY(word)) allWords.push(word);
+                            
                         }
                     }
                 }
             }
         }
     }
-
-   
     console.log("all words");
     console.log(allWords)
     console.log("end here!")
 
-    // copy the contents of the array into allWords.txt
-    // fs.writeFile('allWords.txt', allWords.toString(), function (err) {
-    //     if (err) throw err;
-    //     console.log('Array has been pasted into the file!');
-    //     });
-    
-
-    // clear the contents of the file wordTest.txt
-    // fs.writeFile('wordTest.txt', "", function (err) {
-    //     if (err) throw err;
-    //     //console.log('File has been cleared!');
-    //     });
-    // remove the word if it is not in the dictionary
     for (let i = 0; i < allWords.length; i++) {
         let word = allWords[i].toLowerCase();
-        // check if word has a vowel
-        if (word.length < 3 || word.length > 6 || !/[aeiou]/.test(word)) continue;
-        // var url = "https://api.dictionaryapi.dev/api/v2/entries/en/"+word;
-        // //console.log(url)
-        // await axios.get(url)
-        // .then(response => {
-        //     if (response.data.title === "No Definitions Found") {
-        //         console.log("ERRORING")
-                
-        //     }
-        //     else {
-        //         if (word.length == 3 && !threeLetterWords.includes(word)) threeLetterWords.push(word);
-        //         if (word.length == 4 && !fourLetterWords.includes(word)) fourLetterWords.push(word);
-        //         if (word.length == 5 && !fiveLetterWords.includes(word)) fiveLetterWords.push(word);
-        //         if (word.length == 6 && !sixLetterWords.includes(word)) sixLetterWords.push(word);
-        //     }
-            
-        // })
-        // .catch(error => {
-        //     // paste the word into the file wordTest.txt followed by a newline
-        //     fs.appendFile('wordTest.txt', "\n"+ url , function (err) {
-        //         if (err) throw err;
-        //         //console.log('Word has been appended to the file!');
-        //         });
-            
-        // });
         if (words.check(word)) {
             if (word.length == 3 && !threeLetterWords.includes(word)) threeLetterWords.push(word);
             if (word.length == 4 && !fourLetterWords.includes(word)) fourLetterWords.push(word);
             if (word.length == 5 && !fiveLetterWords.includes(word)) fiveLetterWords.push(word);
             if (word.length == 6 && !sixLetterWords.includes(word)) sixLetterWords.push(word);
         }
-
-        // watch for error
-
-        
+  
     }
     console.log(threeLetterWords);
     console.log(fourLetterWords);
     console.log(fiveLetterWords);
     console.log(sixLetterWords);
     res.send({threeLetterWords, fourLetterWords, fiveLetterWords, sixLetterWords});
-        
-
-    
-
 });
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(3000, function()  {
     console.log("Server is running on port 3000");
 });
+
